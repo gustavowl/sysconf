1c52354928b6faf0f75fe01575793c9f4cd4b488
Koruja <gustavowl@lcc.ufrn.br>
2019-04-06 12:44:10 -0300

    Creates vi.sh script to automatically configure vim

---
+++ vi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Downloads vimrc
+wget -P ~/.vimrc https://raw.githubusercontent.com/gustavowl/sysconf/master/vimrc
+
+#Downloads vim plug
+mkdir -p ~/.vim/autoload
+wget -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Downloads C and C++ syntax highlighting
+git clone https://github.com/octol/vim-cpp-enhanced-highlight.git /tmp/vim-cpp-enhanced-highlight
+mkdir -p ~/.vim/after/syntax/
+mv /tmp/vim-cpp-enhanced-highlight/after/syntax/cpp.vim ~/.vim/after/syntax/cpp.vim
+rm -rf /tmp/vim-cpp-enhanced-highlight
+
+# Run the scripts
+vim +'PlugInstall --sync' +qa
+python3 ~/.vim/plugged/YouCompleteMe/install.py --clang-completer --all
